3153cb7a370015497fcb12fe558ee7b10479d86f
Ben Stone <benjamin.h.stone@gmail.com>
2019-10-07 16:29:10 -0400

    Update lab.js

--- lab.js
+++ lab.js
@@ -31,8 +31,9 @@ let calAndDanAppleCount /*= Your code here */
 // d. Set all the values in applesObj to 0
 
 // Your code here
-//for (value of applesObj) {
-//    assert(value === 0)
+
+//for (key in applesObj) {
+//   assert(applesObj[key] === 0)
 //}
 
 
